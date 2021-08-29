26c92e2a6126f7c937b45f0fde82e161d89d0fab
Igor <50257429+igorechek06@users.noreply.github.com>
2021-08-29 11:03:30 +0900

    Bug fix

--- handlers/generate.py
+++ handlers/generate.py
@@ -4,7 +4,7 @@ from aiogram import types as t
 from utils import filters as f
 
 
-@dp.message_handler(f.message.chance(10), f.message.is_chat)
+@dp.message_handler(f.message.chance(10), f.message.is_chat, content_types=[t.ContentType.ANY])
 async def срать_сообщение_с_шансом(msg: t.Message):
     await сгенерировать_хуету(msg)
 
--- handlers/on_message.py
+++ handlers/on_message.py
@@ -3,8 +3,7 @@ from aiogram import types as t
 from utils import filters as f
 
 
-async def sosalka(msg: t.Message):
-    '''сосет сообщения'''
+async def сосалка(msg: t.Message):
     text = msg.text or msg.caption
     if text.startswith('/'):
         return False
@@ -13,7 +12,6 @@ async def sosalka(msg: t.Message):
     return False
 
 
-@dp.message_handler(f.message.is_chat, f.message.has_text, sosalka, content_types=[t.ContentType.ANY])
-async def НАХУЯ_ПРАВДА_Я_НЕ_ЗНАЮ_ЗАЧЕМ_ЭТА_ФУНКЦИЯ_НУЖНА():
-    print('NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER ')
-    return 'я могу сюда любую хуйню написать, все равно в фильтре фолз =)))))))))))))))))00'
+@dp.message_handler(f.message.is_chat, f.message.has_text, сосалка, content_types=[t.ContentType.ANY])
+async def ХУЙ():
+    pass
