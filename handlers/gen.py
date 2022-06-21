6f325567b24f5ec4a2a6a989c20cb18a91f3bcf4
Igor Tolmachov <me@igorek.dev>
2022-06-21 22:38:31 +0900

    Убрал реквесты, телеграм добавил свою фичу

--- handlers/gen.py
+++ handlers/gen.py
@@ -2,7 +2,7 @@ import mc
 from aiogram import types as t
 
 from shared import instances as ins
-from shared.instances import dp, bot
+from shared.instances import bot, dp
 from utils import filters as f
 
 
@@ -36,16 +36,20 @@ async def изменить_шанс_срания(msg: t.Message):
 
             await msg.answer(f"Теперь я сру с шансом в: {chance}%")
         except Exception:
-            await msg.answer("Я хз что не так, но я знаю что ты дебил \n    /chance <ЧИСЛО ОТ 0 ДО 100>")
+            await msg.answer(
+                "Я хз что не так, но я знаю что ты дебил \n    /chance <ЧИСЛО ОТ 0 ДО 100>"
+            )
     else:
         await msg.answer(f"Я сру с шансом в: {ins.gen_chance}%")
 
 
-@dp.message_handler(f.message.chance, f.message.is_chat, content_types=[t.ContentType.ANY])
+@dp.message_handler(
+    f.message.chance, f.message.is_chat, content_types=[t.ContentType.ANY]
+)
 async def срать_сообщение_с_шансом(msg: t.Message):
     await msg.answer(получить_говно())
 
 
 def получить_говно() -> str:
-    samples = mc.util.load_txt_samples('samples.txt', separator='§')
+    samples = mc.util.load_txt_samples("samples.txt", separator="§")
     return mc.StringGenerator(samples=samples).generate_string().capitalize()
